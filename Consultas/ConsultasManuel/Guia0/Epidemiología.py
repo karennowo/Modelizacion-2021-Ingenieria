8b4d22366ab2c9c06de19a149ce53850f0f9741c
mariano Forti <marianodforti@gmail.com>
2021-04-02 19:48:22 +0200

    mejor epidemiología
    falta ver porque da raro
    
    Update README.md
    
    todo consultas de manuel

--- Consultas/ConsultasManuel/Guia0/Epidemiología.py
+++ Consultas/ConsultasManuel/Guia0/Epidemiología.py
@@ -15,20 +15,18 @@ def funcion (i):
 	dR = lambda t,S,I,R: 0.1*I
 	Der = [dS, dI, dR]
 
+        #MDF-COMMENT lo que estar retornando es la función !
 	return Der[i]
 
-V#MDF-COMMENT a ver si me sale.
-def funcionv(Y):
+#MDF-COMMENT a ver si me sale.
+def funcionv(Y, _gamma, _beta):
+#MDF-COMMENT a demas te conviene poner como argumentos gamma y beta, que son los parametros del modelo
     """
     Parámetros
     ==========
     Y = [S, I, R]
     """
-    dY = [ 
-            -0.35*Y[1]*Y[0],
-            0.35*Y[1]*Y[0]-0.1*Y[1],
-            0.1*Y[1]
-        ]
+    dY = np.array( [ -_gamma*Y[1]*Y[0],  _gamma*Y[1]*Y[0]-_beta*Y[1],  _beta*Y[1]  ]).reshape(-1,1)
     return dY
 
 
@@ -41,17 +39,30 @@ Y[1,0] = I0
 Y[2,0] = R0
 
 Espaciado = t[1] - t[0]
+#MDF-COMMENT 
+Espaciado = np.diff(t)
 
-for i in range(1,len(t)):
-	for j in range(0,3):
-		G = funcion(j)
-		k[0] = G(t[i-1], Y[0,i-1], Y[1,i-1], Y[2,i-1])
-		k[1] = G(t[i-1] + Espaciado/2 , Y[0,i-1] + k[0]*Espaciado/2, Y[1,i-1] + k[0]*Espaciado/2, Y[2,i-1] + k[0]*Espaciado/2)
-		k[2] = G(t[i-1] + Espaciado/2 , Y[0,i-1] + k[1]*Espaciado/2, Y[1,i-1] + k[1]*Espaciado/2, Y[2,i-1] + k[1]*Espaciado/2)
-		k[3] = G(t[i-1] + Espaciado, Y[0,i-1] +k[2]*Espaciado, Y[1,i-1] +k[2]*Espaciado, Y[2,i-1] +k[2]*Espaciado)
-		Y[j,i] = Y[j,i-1] + 1/6*(k[0] + 2*k[1] + 2*k[2] + k[3])*Espaciado
+gamma = 0.5
+beta = 0.01
+for i in range(1,len(t)-1):
+#    for j in range(0,3):
+    K1 = funcionv(Y[:, i-1], gamma, beta)
+    K2 = funcionv(Y[:, i-1].reshape(-1,1)+K1*Espaciado[i]*0.5, gamma, beta)
+    K3 = funcionv( Y[:, i-1].reshape(-1,1) +K2*Espaciado[i]*0.5, gamma, beta)
+    K4 = funcionv( Y[:, i-1].reshape(-1,1) +K3*Espaciado[i],  gamma, beta)
+    Y[:, i] = (Y[:, i-1].reshape(-1,1) + (1/6)*Espaciado[i]*(K1+2*K2*2*K3+K4)).ravel()
+#MDF-COMMENT lo que habías hecho no estaba mal, pero esto de arriva es un poco menos dificil de leer,
+#MDF-COMMENT lo unico que hace un poco de lío es el tema de que lo definiste todo como vector columna
+#            G = funcionv(Y[:,-1])
+#MDF-COMMENT            G = funcion(j)
+#MDF-COMMENT            k[0] = G(t[i-1], Y[0,i-1], Y[1,i-1], Y[2,i-1])
+#MDF-COMMENT            k[1] = G(t[i-1] + Espaciado/2 , Y[0,i-1] + k[0]*Espaciado/2, Y[1,i-1] + k[0]*Espaciado/2, Y[2,i-1] + k[0]*Espaciado/2)
+#MDF-COMMENT            k[2] = G(t[i-1] + Espaciado/2 , Y[0,i-1] + k[1]*Espaciado/2, Y[1,i-1] + k[1]*Espaciado/2, Y[2,i-1] + k[1]*Espaciado/2)
+#MDF-COMMENT            k[3] = G(t[i-1] + Espaciado, Y[0,i-1] +k[2]*Espaciado, Y[1,i-1] +k[2]*Espaciado, Y[2,i-1] +k[2]*Espaciado)
+#MDF-COMMENT            Y[j,i] = Y[j,i-1] + 1/6*(k[0] + 2*k[1] + 2*k[2] + k[3])*Espaciado
 
 plt.plot(t,Y[0,:])
 plt.plot(t,Y[1,:])
 plt.plot(t,Y[2,:])
-plt.show()
+#MDF-COMMENT  plt.show()
+#MDF-COMMENT a demas de mostrar el grafico, podes guardarlo en un archivo
